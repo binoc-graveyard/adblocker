52d44ca738453e44b143be17e93970575da09a93
Matt A. Tobin <email@mattatobin.com>
2016-06-14 22:16:31 -0400

    Add the ADDON_URL homepage variable to metadata and ppDefines and reflect this in install.rdf

--- config/metadata.cmake
+++ config/metadata.cmake
@@ -6,6 +6,7 @@ set(ADDON_XPI_NAME "adblocklatitude")
 set(ADDON_SHORT_DESC "Acceptable ads were yesterday!")
 set(ADDON_LONG_DESC "This is the Next Generation Adblock Latitude. <br /><br /> Please disable or remove any previous versions of the Adblock Family before installing. You may want to make a backup of your filter presets via the Filter Preferences window so that you can reimport them if something goes wrong.")
 set(ADDON_AUTHOR "Pale Moon Add-ons Team")
+set(ADDON_URL "https://addons.palemoon.org/extensions/privacy-and-security/adblock-latitude/")
 set(ADDON_TARGET_APP_NAME "Pale Moon")
 set(ADDON_TARGET_APP_ID "{8de7fcbb-c55c-4fbe-bfc5-fc555c87dbc4}")
 set(ADDON_TARGET_APP_MINVER "25.0")
@@ -22,6 +23,7 @@ set(ppDefines
     -DADDON_SHORT_DESC="${ADDON_SHORT_DESC}"
     -DADDON_LONG_DESC="${ADDON_LONG_DESC}"
     -DADDON_AUTHOR="${ADDON_AUTHOR}"
+    -DADDON_URL="${ADDON_URL}"
     -DADDON_TARGET_APP_NAME="${ADDON_TARGET_APP_NAME}"
     -DADDON_TARGET_APP_ID="${ADDON_TARGET_APP_ID}"
     -DADDON_TARGET_APP_MINVER="${ADDON_TARGET_APP_MINVER}"
